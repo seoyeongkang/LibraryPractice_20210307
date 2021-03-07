41d48fa313060ddeb275f15d75609ee997f754ca
seoyeongkang <jumprope123@naver.com>
2021-03-07 15:14:43 +0900

    프사 크게 보는 액티비티 추가

---
+++ app/src/main/java/com/tjoeun/librarypractice_20210307/ViewPhotoActivity.kt
@@ -0,0 +1,22 @@
+package com.tjoeun.librarypractice_20210307
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_photo)
+        setUpEvents()
+        setValues()
+}
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
