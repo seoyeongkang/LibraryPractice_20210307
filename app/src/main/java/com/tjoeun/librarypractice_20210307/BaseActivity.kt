7a08854461249b492dee955735e88acf07156810
seoyeongkang <jumprope123@naver.com>
2021-03-07 14:34:53 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/librarypractice_20210307/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.tjoeun.librarypractice_20210307
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setUpEvents()
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/tjoeun/librarypractice_20210307/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.tjoeun.librarypractice_20210307
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setUpEvents()
+        setValues()
+
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
